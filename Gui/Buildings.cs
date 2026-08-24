1c94a2e9bccf8a5f147b95e359d245134e9e8bae
Geistvk <144540427+Geistvk@users.noreply.github.com>
2026-08-25 00:02:06 +0200

    Added an early Version of 3d Models
    Added an early version of 3d Model rendering
    Reworked the GUIS.
    Added more Statistics to the Top Left Corner

--- Gui/Buildings.cs
+++ Gui/Buildings.cs
@@ -21,6 +21,18 @@ namespace NexaForge
         private Building _building;
         private readonly VoxelGrid _grid;
 
+        private const int lineHeight = 30;
+        private const int startY = 70;
+        private const int padding = 20;
+        private const int valueOffset = 140;
+        private const int border = 2;
+
+        // Abstände
+        private const int titleTopPadding = 18;
+        private const int separatorOffset = 50;
+        private const int bottomPadding = 20;
+        private const int noticeSpacing = 15;
+
         public bool IsOpen { get; private set; }
 
         public BuildingsGui(
@@ -52,9 +64,9 @@ namespace NexaForge
 
         public void Draw(GraphicsDevice graphicsDevice)
         {
-            if (!IsOpen || _building == null ||
-                _grid.Get(_building.GridX, _building.GridZ) != _building ||
-                _grid.Get(_building.GridX, _building.GridZ) == null)
+            if (!IsOpen ||
+                _building == null ||
+                _grid.Get(_building.GridX, _building.GridZ) != _building)
             {
                 Close();
                 return;
@@ -62,8 +74,110 @@ namespace NexaForge
 
             _spriteBatch.Begin();
 
-            int width = 360;
-            int height = 230;
+            Building building = _building switch
+            {
+                Miner => _building as Miner,
+                Belt => _building as Belt,
+                Storage => _building as Storage,
+                _ => null
+            };
+
+            float stored = building switch
+            {
+                Miner miner => miner.OreBuffer,
+                Belt belt => belt.ItemAmount,
+                Storage storage => storage.Stored,
+                _ => 0f
+            };
+
+            string title = _building.Type.ToString();
+
+            string[,] infos = {
+                { "Building", _building.Type.ToString() },
+                { "Content", $"{stored:0.0} Ore" },
+                { "Status", _building.Status },
+                { "Grid X", _building.GridX.ToString() },
+                { "Grid Z", _building.GridZ.ToString() },
+                {
+                    "Position",
+                    $"{_building.Transform.Position.X:0.0} | " +
+                    $"{_building.Transform.Position.Y:0.0} | " +
+                    $"{_building.Transform.Position.Z:0.0}"
+                }
+            };
+
+            string notice = "Press E to Close this GUI";
+
+            // ------------------------------------------------------------
+            // Dynamische Größe berechnen
+            // ------------------------------------------------------------
+
+            int infoCount = infos.GetLength(0);
+
+            Vector2 titleSize = _font.MeasureString(title);
+            Vector2 noticeSize = _font.MeasureString(notice);
+
+            float maxNameWidth = 0;
+            float maxValueWidth = 0;
+
+            for (int i = 0; i < infoCount; i++)
+            {
+                Vector2 nameSize = _font.MeasureString(infos[i, 0]);
+                Vector2 valueSize = _font.MeasureString(infos[i, 1]);
+
+                if (nameSize.X > maxNameWidth)
+                    maxNameWidth = nameSize.X;
+
+                if (valueSize.X > maxValueWidth)
+                    maxValueWidth = valueSize.X;
+            }
+
+            // Breite der beiden Spalten
+            int columnGap = 25;
+
+            int contentWidth =
+                padding +
+                (int)Math.Max(titleSize.X, maxNameWidth + columnGap + maxValueWidth) +
+                padding;
+
+            // Notice muss ebenfalls hineinpassen
+            int noticeWidth = padding + (int)noticeSize.X + padding;
+
+            int width = Math.Max(contentWidth, noticeWidth);
+
+            // Mindestbreite
+            width = Math.Max(width, 300);
+
+            // ------------------------------------------------------------
+            // Dynamische Höhe
+            // ------------------------------------------------------------
+
+            int titleHeight = (int)titleSize.Y;
+
+            int titleAreaHeight =
+                titleTopPadding +
+                titleHeight +
+                12;
+
+            int separatorHeight = 1;
+
+            int infoAreaHeight =
+                infoCount * lineHeight;
+
+            int noticeHeight = (int)noticeSize.Y;
+
+            int height =
+                titleAreaHeight +
+                separatorHeight +
+                noticeSpacing +
+                infoAreaHeight +
+                noticeSpacing +
+                noticeHeight +
+                bottomPadding;
+
+            // ------------------------------------------------------------
+            // Position
+            // ------------------------------------------------------------
 
             int x = graphicsDevice.Viewport.Width - width - 20;
             int y = 60;
@@ -75,121 +189,97 @@ namespace NexaForge
                 height
             );
 
+            // ------------------------------------------------------------
             // Background
+            // ------------------------------------------------------------
+
             _spriteBatch.Draw(
                 _pixel,
                 window,
                 Color.Black
             );
 
+            // ------------------------------------------------------------
             // Border
-            const int border = 2;
+            // ------------------------------------------------------------
 
-            _spriteBatch.Draw(
-                _pixel,
-                new Rectangle(
-                    x,
-                    y,
-                    width,
-                    border
-                ),
+            DrawBorder(
+                x,
+                y,
+                width,
+                height,
+                border,
                 Color.Purple
             );
 
-            _spriteBatch.Draw(
-                _pixel,
-                new Rectangle(
-                    x,
-                    y + height - border,
-                    width,
-                    border
-                ),
-                Color.Purple
-            );
-
-            _spriteBatch.Draw(
-                _pixel,
-                new Rectangle(
-                    x,
-                    y,
-                    border,
-                    height
-                ),
-                Color.Purple
-            );
-
-            _spriteBatch.Draw(
-                _pixel,
-                new Rectangle(
-                    x + width - border,
-                    y,
-                    border,
-                    height
-                ),
-                Color.Purple
-            );
-
+            // ------------------------------------------------------------
             // Title
+            // ------------------------------------------------------------
+
+            int titleY = y + titleTopPadding;
+
             _spriteBatch.DrawString(
                 _font,
-                _building.Type.ToString(),
+                title,
                 new Vector2(
-                    x + 20,
-                    y + 18
+                    x + padding,
+                    titleY
                 ),
                 Color.White
             );
 
+            // ------------------------------------------------------------
             // Separator
+            // ------------------------------------------------------------
+
+            int separatorY = y + separatorOffset;
+
             _spriteBatch.Draw(
                 _pixel,
                 new Rectangle(
-                    x + 20,
-                    y + 50,
-                    width - 40,
-                    1
+                    x + padding,
+                    separatorY,
+                    width - padding * 2,
+                    separatorHeight
                 ),
                 Color.Gray
             );
 
+            // ------------------------------------------------------------
             // Information
-            DrawInfo(
-                x + 20,
-                y + 70,
-                "Building",
-                _building.Type.ToString()
-            );
+            // ------------------------------------------------------------
 
-            DrawInfo(
-                x + 20,
-                y + 100,
-                "Grid X",
-                _building.GridX.ToString()
-            );
+            int infoStartY =
+                separatorY +
+                separatorHeight +
+                noticeSpacing;
 
-            DrawInfo(
-                x + 20,
-                y + 130,
-                "Grid Z",
-                _building.GridZ.ToString()
-            );
+            for (int i = 0; i < infoCount; i++)
+            {
+                DrawInfo(
+                    x,
+                    infoStartY,
+                    infos[i, 0],
+                    infos[i, 1],
+                    i
+                );
+            }
 
-            DrawInfo(
-                x + 20,
-                y + 160,
-                "Position",
-                $"{_building.Transform.Position.X:0.0} | " +
-                $"{_building.Transform.Position.Y:0.0} | " +
-                $"{_building.Transform.Position.Z:0.0}"
-            );
-
+            // ------------------------------------------------------------
             // Notice
+            // ------------------------------------------------------------
+
+            int noticeY =
+                infoStartY +
+                infoAreaHeight +
+                noticeSpacing;
+
             _spriteBatch.DrawString(
                 _font,
-                "Press E to Close this GUI",
+                notice,
                 new Vector2(
-                    x + 20,
-                    y + 195
+                    x + padding,
+                    noticeY
                 ),
                 Color.Gray
             );
@@ -202,22 +292,88 @@ namespace NexaForge
 
         private void DrawInfo(
             int x,
-            int y,
+            int startY,
             string name,
-            string value)
+            string value,
+            int index)
         {
+            int y = startY + index * lineHeight;
+
             _spriteBatch.DrawString(
                 _font,
                 name,
-                new Vector2(x, y),
+                new Vector2(
+                    x + padding,
+                    y
+                ),
                 Color.LightGray
             );
 
             _spriteBatch.DrawString(
                 _font,
                 value,
-                new Vector2(x + 120, y),
+                new Vector2(
+                    x + valueOffset,
+                    y
+                ),
                 Color.White
+            );
+        }
+
+        private void DrawBorder(
+            int x,
+            int y,
+            int width,
+            int height,
+            int thickness,
+            Color color)
+        {
+            // Oben
+            _spriteBatch.Draw(
+                _pixel,
+                new Rectangle(
+                    x,
+                    y,
+                    width,
+                    thickness
+                ),
+                color
+            );
+
+            // Unten
+            _spriteBatch.Draw(
+                _pixel,
+                new Rectangle(
+                    x,
+                    y + height - thickness,
+                    width,
+                    thickness
+                ),
+                color
+            );
+
+            // Links
+            _spriteBatch.Draw(
+                _pixel,
+                new Rectangle(
+                    x,
+                    y,
+                    thickness,
+                    height
+                ),
+                color
+            );
+
+            // Rechts
+            _spriteBatch.Draw(
+                _pixel,
+                new Rectangle(
+                    x + width - thickness,
+                    y,
+                    thickness,
+                    height
+                ),
+                color
             );
         }
     }
